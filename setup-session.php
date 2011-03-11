a59033e7570162bb09781aa4a31ff6a106e3502d
Jesse Jacobsen <jmatjac@gmail.com>
2011-03-11 11:15:24 -0800

    Added setup-session

---
+++ setup-session.php
@@ -0,0 +1,8 @@
+<?
+session_set_cookie_params(604800);
+session_start();
+$sprefix = realpath(dirname(__FILENAME__));
+if (! (array_key_exists($sprefix, $_SESSION) && is_array($_SESSION))) {
+    $_SESSION[$sprefix] = array();
+}
+?>
