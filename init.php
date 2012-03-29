04d0a1686635cbc647a44c5505815038a4b15e93
Jesse Jacobsen <jmatjac@gmail.com>
2012-03-29 10:49:09 -0700

    Rearranged init.php for dependencies

--- init.php
+++ init.php
@@ -7,6 +7,11 @@ require("./setup-session.php");
 require("./functions.php");
 $serverdir = dirname($_SERVER['PHP_SELF']);
 $script_basename = basename($_SERVER['PHP_SELF'], '.php');
+if ((! file_exists("db-connection.php") and
+    (! is_link($_SERVER['SCRIPT_FILENAME'])))) {
+        header("Location: {$serverdir}/utility/setup-dbconfig.php");
+        exit(0);
+}
 $upgradedb = false;
 if (! file_exists("dbversion.txt")) {
     $upgradedb = true;
@@ -20,11 +25,6 @@ if (! file_exists("dbversion.txt")) {
         $upgradedb = true;
         $oldversion = "{$dbcurrent[0]}.{$dbcurrent[1]}";
     }
-}
-if ((! file_exists("db-connection.php") and
-    (! is_link($_SERVER['SCRIPT_FILENAME'])))) {
-        header("Location: {$serverdir}/utility/setup-dbconfig.php");
-        exit(0);
 }
 if ($upgradedb) {
     $newversion = "{$version['major']}.{$version['minor']}";
