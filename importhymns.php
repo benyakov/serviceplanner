7ba00f83af109a453f3e3b0b73d70dafab6e56f7
Jesse Jacobsen <jmatjac@gmail.com>
2012-03-28 17:50:59 -0700

    Getting rowcount from exec in import script

--- importhymns.php
+++ importhymns.php
@@ -11,11 +11,11 @@ if ($_POST['prefix'] && strpos($_POST['prefix'], ' ') === false) {
         header('Location: admin.php');
         exit(0);
     }
-    $q = $dbh->exec("INSERT INTO `{$dbp}names` AS n1
+    $rowcount = $dbh->exec("INSERT INTO `{$dbp}names` AS n1
         COLUMNS (book, number, title)
         (SELECT book, number, title FROM `{$_POST['prefix']}names` AS n2
             WHERE (NOT (n1.book = n2.book AND n1.number = n2.number)))");
-    setMessage($q->rowCount() . " hymn names imported.");
+    setMessage($rowcount . " hymn names imported.");
     header('Location: admin.php');
 } else {
     setMessage("Bad prefix: `{$_POST['prefix']}'");
