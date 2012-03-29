e9c11de101dcc551a17a6c166855442e226925fa
Jesse Jacobsen <jmatjac@gmail.com>
2012-03-29 07:17:44 -0700

    Draft of auto db-upgrade in init.php

--- init.php
+++ init.php
@@ -5,6 +5,25 @@ require("./setup-session.php");
 require("./functions.php");
 $serverdir = dirname($_SERVER['PHP_SELF']);
 $script_basename = basename($_SERVER['PHP_SELF'], '.php');
+$upgradedb = false;
+if (! file_exists("dbversion.txt")) {
+    $upgradedb = true;
+    $oldversion = "";
+} else {
+    $fh = fopen($dbversion, "rb");
+    $dbcurrent = explode('.', trim(fread($fh, 64)));
+    fclose($fh);
+    if (! ($version['major'] == $dbcurrent[0]
+        && $version['minor'] == $dbcurrent[1])) {
+        $upgradedb = true;
+        $oldversion = "{$dbcurrent[0]}.{$dbcurrent[1]}";
+    }
+}
+if ($upgradedb) {
+    $newversion = "{$version['major']}.{$version['minor']}";
+    header("Location: {$serverdir}/utility/upgrades/{$oldversion}to{$newversion}.php");
+    exit(0);
+}
 if ((! file_exists("db-connection.php") and
     (! is_link($_SERVER['SCRIPT_FILENAME'])))) {
         header("Location: {$serverdir}/utility/setup-dbconfig.php");
