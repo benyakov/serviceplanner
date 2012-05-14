69b81795388244ad29cc37a9311ad8ed0b5f903c
Jesse Jacobsen <jmatjac@gmail.com>
2012-05-14 08:23:52 -0700

    Script for editing church year day parameters

---
+++ churchyear.php
@@ -0,0 +1,113 @@
+<?
+/* Edit the days in the church year
+ */
+require("init.php");
+if (! $auth) {
+    setMessage("Access denied.  Please log in.");
+    header("location: index.php");
+    exit(0);
+}
+
+if (! $dbh->query("SELECT 1 FROM `{$dbp}churchyear`")) {
+    /* Create the church year table */
+    $q = $dbh->prepare("CREATE TABLE `{$dbp}churchyear` (
+        `season` varchar default NULL,
+        `dayname` varchar NOT NULL,
+        `base` varchar default NULL,
+        `offset` smallint default 0,
+        `month` tinyint default 0,
+        `day`   tinyint default 0,
+        `observed_month` tinyint default 0,
+        `observed_sunday` tinyint default 0,
+        KEY `dayname` (`dayname`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8") ;
+    $q->execute() or die(array_pop($q->errorInfo()));
+    $dbh->beginTransaction();
+    $fh = fopen("historictable.csv", "r");
+    $headings = fgetcsv($fh);
+    while (($record = fgetcsv($fh, 250)) != FALSE) {
+        $r = array();
+        $record = quote_array($record);
+        foreach ($record as $field) {
+            $f = trim($field);
+            if (! $f) {
+                $f = "NULL";
+            } elseif (! is_numeric($f)) {
+                $f = "'$f'";
+            }
+            $r[] = $f;
+        }
+        $q = $dbh->prepare("INSERT INTO {$dbp}xref (season, dayname, base, offset, month, day, observed_month, observed_sunday)
+            VALUES ({$r[0]}, {$r[1]}, {$r[2]}, {$r[3]}, {$r[4]}, {$r[5]}, {$r[6]}, {$r[7]})");
+        $q->execute() or dieWithRollback($q, "\n".__FILE__.":".__LINE__);
+    }
+    $dbh->commit();
+}
+
+if ($_GET['dayname']) {
+    $q = $dbh->prepare("SELECT `season`, `base`, `offset`, `month`, `day`, `observed_month`, `observed_sunday`
+        FROM `{$dbp}churchyear`
+        WHERE `dayname` = :dayname");
+    $q->bindParam(":dayname", $_GET['dayname']);
+    $q->execute();
+    $specifics = $q->fetch(PDO::FETCH_ASSOC);
+    /* Show a day edit form with dates in the surrounding 10 years */
+?>
+    <div id="edit-day">
+    <form id="dayform" name="dayform" method="post">
+        <input type="hidden" name="submit-day" value="1">
+        <dl>
+        <dt><label for="dayname">Day Name</label></dt>
+        <dd><input type="text" name="dayname" id="dayname"
+            value="<?=$specifics['dayname']?>"></dd>
+        <dt><label for="season">Season</label></dt>
+        <dd><input type="text" name="season" id="season"
+            value="<?=$specifics['season']?>"></dd>
+        <dt><label for="base">Base Moveable Day</label></dt>
+        <dd><select name="base" id="base">
+            <option value="None">None</option>
+            <option value="Easter"
+                <?=$specifics['base']=="Easter"?"selected=\"selected\"":""?>>Easter</option>
+            <option value="Christmas 1"
+                <?=$specifics['base']=="Christmas 1"?"selected=\"selected\"":""?>>Christmas 1</option>
+            <option value="Michaelmas 1"
+                <?=$specifics['base']=="Michaelmas 1"?"selected=\"selected\"":""?>>Michaelmas 1</option>
+            </select></dd>
+        <dt><label for="offset">Offset from Base in Days</label></dt>
+        <dd><input type="number" name="offset" id="offset"
+            value="<?=$specifics['offset']?>"></dd>
+        <dt><label for="month">Month</label></dt>
+        <dd><input name="month" id="month" type="number" min="0" max="12"
+            value="<?=$specifics['month']?>"></dd>
+        <dt><label for="day">Day of Month</label></dt>
+        <dd><input name="day" id="day" type="number" min="0" max="31"
+            value="<?=$specifics['day']?>"></dd>
+        <dt><label for="observed-month">Observed Month</label></dt>
+        <dd><input name="observed-month" id="observed-month"
+            type="number" min="0" max="12"
+            value="<?=$specifics['observed_month']?>"></dd>
+        <dt><label for="observed-sunday">Observed Sunday</label></dt>
+        <dd><input name="observed-sunday" id="observed-sunday"
+            type="number" min="0" max="31"
+            value="<?=$specifics['observed_sunday']?>"></dd>
+        </dl>
+        <button type="submit" name="submit">Submit</button>
+        <button type="reset" name="reset">Reset</button>
+    </form>
+    <p>Calculated dates include:
+<?
+    $thisyear = date('Y');
+    for ($y = $thisyear-5; $y<=$thisyear+5; $y++) {
+        echo date("mdY", get_date_for($_GET['dayname'], $y, $specifics));
+    }
+    echo "</p>";
+}
+
+if ($_POST['submit-day']==1) {
+    // Update/save supplied values for the given day
+
+}
+
+
+
+?>
