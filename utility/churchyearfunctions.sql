19c53ffe46d119f068328e2d9f0ce6bbdc8c8c48
Jesse Jacobsen <jmatjac@gmail.com>
2012-05-24 12:04:36 -0700

    Updated syntax in churchyearfunctions.sql

--- utility/churchyearfunctions.sql
+++ utility/churchyearfunctions.sql
@@ -1,9 +1,9 @@
-DELIMITER $$;
+DELIMITER $$
 CREATE FUNCTION easter_in_year(p_year INTEGER) RETURNS DATE
 DETERMINISTIC
 BEGIN
     DECLARE century, shiftedEpact, adjustedEpact INTEGER;
-    DECLARE paschalMoon DATE;
+    DECLARE paschalMoon, apr19 DATE;
     SET century = 1 + p_year/100;
      # "Age of moon for April 5";
     SET shiftedEpact = (14 + 11 * (p_year % 19)) # Nicean rule
@@ -16,9 +16,9 @@ BEGIN
     ELSE
         SET adjustedEpact = shiftedEpact;
     END IF;
-    SET paschalMoon = DATE_SUB(CONCAT_WS('-', p_year, 4, 19),
-        INTERVAL adjustedEpact DAYS);
-    RETURN DATE_ADD(paschalMoon, INTERVAL (8 - DAYOFWEEK(paschalMoon)) DAYS);
+    SET apr19 = CONCAT_WS('-', p_year, 4, 19) ;
+    SET paschalMoon = apr19 - INTERVAL adjustedEpact DAY;
+    RETURN paschalMoon + INTERVAL (8 - DAYOFWEEK(paschalMoon)) DAY;
 END$$
 
 CREATE FUNCTION christmas1_in_year(p_year INTEGER) RETURNS DATE
@@ -29,7 +29,7 @@ BEGIN
     IF wdchristmas = 1 THEN
         RETURN CONCAT_WS('-', p_year, 12, 25);
     ELSE
-        RETURN DATE_ADD(CONCAT_WS('-', p_year, 12, 25), 8-wdchristmas);
+        RETURN CONCAT_WS('-', p_year, 12, 25) INTERVAL 8-wdchristmas DAY;
     END IF;
 END$$
 
@@ -38,16 +38,16 @@ DETERMINISTIC
 BEGIN
     DECLARE mike_observed, wdmichaelmas, oct1wd INTEGER
     DECLARE michaelmas, oct1 DATE;
-    SELECT observed_sunday FROM {$dbp}churchyear
-        WHERE dayname="Michaelmas" INTO @mike_observed;
-    SET @michaelmas = STR_TO_DATE(CONCAT_WS('-', p_year, 9, 29));
-    SET @wdmichaelmas = DAYOFWEEK(@michaelmas);
-    IF @mike_observed != -1 && @wdmichaelmas = 7 THEN
+    SELECT observed_sunday FROM `{$dbp}churchyear`
+        WHERE dayname="Michaelmas" INTO mike_observed;
+    SET michaelmas = STR_TO_DATE(CONCAT_WS('-', p_year, 9, 29));
+    SET wdmichaelmas = DAYOFWEEK(michaelmas);
+    IF mike_observed != -1 && wdmichaelmas = 7 THEN
         RETURN CONCAT_WS('-', p_year, 9, 30);
     END IF;
-    SET @oct1 = CONCAT_WS('-', p_year, 10, 1);
-    SET @oct1wd = DAYOFWEEK(@oct1);
-    RETURN DATE_ADD(@oct1, 8-@oct1wd DAYS);
+    SET oct1 = CONCAT_WS('-', p_year, 10, 1);
+    SET oct1wd = DAYOFWEEK(oct1);
+    RETURN oct1 + INTERVAL 8-oct1wd DAY;
 END$$
 
 CREATE FUNCTION calc_date_in_year (p_year INTEGER, p_dayname STRING,
@@ -59,11 +59,11 @@ BEGIN
         RETURN CONCAT_WS('-', p_year, month, day);
     END IF;
     IF base = "Easter" THEN
-        RETURN DATE_ADD(easter_in_year(p_year), offset DAYS);
+        RETURN easter_in_year(p_year) + INTERVAL offset DAY;
     ELSEIF base = "Christmas 1" THEN
-        RETURN DATE_ADD(christmas1_in_year(p_year), offset DAYS);
+        RETURN christmas1_in_year(p_year) + INTERVAL offset DAY;
     ELSEIF base = "Michaelmas 1" THEN
-        RETURN DATE_ADD(michaelmas1_in_year(p_year), offset DAYS);
+        RETURN michaelmas1_in_year(p_year) + INTERVAL offset DAY;
     END IF;
 END$$
 
@@ -73,10 +73,10 @@ RETURNS DATE
 BEGIN
     DECLARE base, offset, month, day INTEGER;
     SELECT base, offset, month, day
-        FROM {$dbp}churchyear
+        FROM `{$dbp}churchyear`
         WHERE dayname=p_dayname
         INTO base, offset, month, day;
-    RETURN calc_date_in_year(p_year, p_dayname, base, offset, month, day)
+    RETURN calc_date_in_year(p_year, p_dayname, base, offset, month, day);
 END$$
 
 CREATE FUNCTION calc_observed_date_in_year (p_year INTEGER, p_dayname STRING,
@@ -87,29 +87,27 @@ BEGIN
     DECLARE actual, firstofmonth, lastofmonth DATE;
     IF base IS NULL THEN
         IF observed_month = 0 THEN
-            SET actual = date_in_year(p_year, p_dayname)
+            SET actual = date_in_year(p_year, p_dayname);
             IF DAYOFWEEK(actual) > 1 THEN
-                RETURN DATE_ADD(actual, 8-DAYOFWEEK(actual) DAYS);
+                RETURN actual + INTERVAL 8-DAYOFWEEK(actual) DAY;
             END IF;
         END IF;
         IF observed_sunday > 0 THEN
             SET firstofmonth =
                 CONCAT_WS('-', p_year, observed_month, 1);
             IF DAYOFWEEK(firstofmonth) > 1 THEN
-                SET firstofmonth = DATE_ADD(firstofmonth,
-                    8-DAYOFWEEK(firstofmonth) DAYS);
+                SET firstofmonth = firstofmonth
+                    + INTERVAL 8-DAYOFWEEK(firstofmonth) DAY;
             END IF;
-            RETURN DATE_ADD(firstofmonth, (observed_sunday-1)*7 DAYS);
+            RETURN firstofmonth + INTERVAL (observed_sunday-1)*7 DAY;
         ELSE
-            SET lastofmonth =
-                DATE_SUB(DATE_ADD(
-                    CONCAT_WS('-', p_year, observed_month)
-                    , 1 MONTH), 1 DAY);
+            SET lastofmonth = (CONCAT_WS('-', p_year, observed_month, 1)
+                + INTERVAL 1 MONTH) - INTERVAL 1 DAY);
             IF DAYOFWEEK(lastofmonth > 1) THEN
-                SET lastofmonth = DATE_SUB(lastofmonth,
-                    DAYOFWEEK(lastofmonth)-1);
+                SET lastofmonth = lastofmonth
+                    - INTERVAL DAYOFWEEK(lastofmonth)-1 DAY;
             END IF;
-            RETURN DATE_ADD(lastofmonth, (observed_sunday+1)*7 DAYS);
+            RETURN lastofmonth + INTERVAL (observed_sunday+1)*7 DAY;
         END IF;
     END IF;
 END$$
@@ -120,7 +118,7 @@ RETURNS DATE
 BEGIN
     DECLARE base, observed_month, observed_sunday INTEGER
 SELECT base, observed_month, observed_sunday
-    FROM {$dbp}churchyear
+    FROM `{$dbp}churchyear`
     WHERE dayname=p_dayname
     INTO base, observed_month, observed_sunday;
 RETURN calc_observed_date_in_year(p_year, p_dayname, base, observed_month,
