81718ae5311a4654ff83268620a531e61987bedd
Jesse Jacobsen <jmatjac@gmail.com>
2012-06-03 13:26:21 -0700

    Added copyright notice to code files

--- hymnindex.php
+++ hymnindex.php
@@ -1,4 +1,29 @@
-<?php
+<? /* Creation/display of hymn cross-reference index
+    Copyright (C) 2012 Jesse Jacobsen
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Send feedback or donations to: Jesse Jacobsen <jmatjac@gmail.com>
+
+    Mailed donation may be sent to:
+    Bethany Lutheran Church
+    2323 E. 12th St.
+    The Dalles, OR 97058
+    USA
+ */
+
 require('./init.php');
 function linksort($text, $sort) {
     // Return a link requesting the given sort
