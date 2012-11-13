53455280a3124f91a70e81bb1c05a23debc1ccfe
Jesse Jacobsen <jmatjac@gmail.com>
2012-11-13 13:07:43 -0800

    Fixed prefix on view names

--- utility/createviews.php
+++ utility/createviews.php
@@ -1,15 +1,15 @@
 <? // Create views needed to process day synonyms
 
-$q = $dbh->exec("DROP VIEW IF EXISTS `synlessons`");
-$q = $dbh->prepare("CREATE VIEW `synlessons` AS
+$q = $dbh->exec("DROP VIEW IF EXISTS `{$dbp}synlessons`");
+$q = $dbh->prepare("CREATE VIEW `{$dbp}synlessons` AS
     SELECT s.synonym AS dayname, l.lectionary, l.lesson1, l.lesson2,
     l.gospel, l.psalm, l.s2lesson, l.s2gospel, l.s3lesson, l.s3gospel,
     l.hymnabc, l.hymn
     FROM `{$dbp}churchyear_lessons` AS l
     RIGHT JOIN `{$dbp}churchyear_synonyms` AS s ON (l.dayname = s.canonical)");
 $q->execute();
-$q = $dbh->exec("DROP VIEW IF EXISTS `synpropers`");
-$q = $dbh->prepare("CREATE VIEW `synpropers` AS
+$q = $dbh->exec("DROP VIEW IF EXISTS `{$dbp}synpropers`");
+$q = $dbh->prepare("CREATE VIEW `{$dbp}synpropers` AS
     SELECT s.synonym AS dayname, p.color, p.theme, p.introit, p.note
     FROM `{$dbp}churchyear_propers` AS p
     RIGHT JOIN `{$dbp}churchyear_synonyms` AS s ON (p.dayname = s.canonical)");
