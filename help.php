28ad8dc780a03cd4f5a917b8554c4abcc03133ef
Pastor <jmatjac@gmail.com>
2013-04-17 16:10:35 -0700

    Adding help document with an intro

---
+++ help.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<?
+require(".functions.php");
+html_head("Help");
+?>
+<body>
+    <h1>Contents</h1>
+    <ul>
+    <li><a href="#tour">Introductory Tour</a></li>
+    </ul>
+
+
+    <h1><a name="tour">Introductory Tour</a></h1>
+
+    <p>So you have your own installation of the service planner, and now
+you'd like to know how to use it.  This introduction should help.  I'll
+try to keep it brief, at the risk of leaving out some important or
+advanced things.</p>
+
+    <p>It will help to have a web browser window open to the service
+planner beside this introduction, so you can try things yourself as your
+read.</p>
+
+    <p>The pages of the service planner are arranged in a series of tabs
+accessible across the top of the window.  Anyone can use the service
+planner, but unless you log in, you will only see a limited number of tabs.
+Login links and related information are at the very top of the page on the
+left side.  Privileged users will also see a "User Administration" link at
+the center of the very top when they are logged in.  That's where you
+manage other users, if you need any.</p>
+
+    <p>On the right of the very top is an Adjust Styles link, which opens a
+dialog box on the screen allowing you to make some adjustments to the
+layout of the service listing.  Unless you are dissatisfied with the
+listing at some point, just ignore that link.</p>
+
+    <p>There are three tabs available to anonymous (not logged-in) users:
+"Upcoming Hymns," "Service Records," and "Cross Ref."  The first simply
+lists all hymns in their services from this day forward, in chronological
+order.  The very next service is at the top of the list.  "Service Records"
+is similar, but it shows <em>all</em> services that have been planned, with
+the latest (farthest in the future) at the top of the list.  The Cross Ref
+tab displays a cross-reference table of hymn numbers from various books.
+Clicking on a blue heading causes the page to reload, sorted on that
+column.</p>
+
+    <p>On the Service Records page (and the soon-to-be-introduced Modify
+Services page), for your convenience, there is a <a name="thisweek">Jump to
+This Week</a> link-button at the top right of the page, which will scroll the
+page to contemporary planned services.  (That's different from planned
+contemporary services, which the service planner does not support.)</p>
+
+    <p>Under the main heading is an adjustable Listing Limit, which allows
+you to specify how many hymns should be listed on the page.  When you get a
+lot of hymns in the service planner, and you load all of them, the page
+refresh can get a mite slow.</p>
+
+    <p>When you log in, the Service Records page is replaced with the much more
+powerful Modify Services page.  This is where you add new services or change
+ones you've already planned.  Two more buttons behind the Listing Limit allow
+you to list only future hymns (like the Upcoming Hymns page) or show all hymns
+(like the Service Records page).</p>
+
+    <p>Each service is listed under its own heading line containing the date
+and location of the service and liturgical day name.  Below that line appear
+the <em>Evangelical Lutheran Hymnary (ELH)</em>'s topical description for
+hymns on that day, as well as the liturgical color for paraments.</p>
+
+    <p>
+
+</body>
+</html>
+
