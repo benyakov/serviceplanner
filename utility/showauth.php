39e5826f613da6f81aeacfe96db9043aecb2e97f
Jesse Jacobsen <jmatjac@gmail.com>
2013-06-01 17:40:00 -0700

    Adding a utility to peek into the session auth data

---
+++ utility/showauth.php
@@ -0,0 +1,45 @@
+<?/* Display the authdata for the current user.
+    Copyright (C) 2013 Jesse Jacobsen
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Send feedback or donations to: Jesse Jacobsen <jmatjac@gmail.com>
+
+    Mailed donation may be sent to:
+    Bethany Lutheran Church
+    2323 E. 12th St.
+    The Dalles, OR 97058
+    USA
+ */
+chdir("..");
+require("./init.php");
+function displayarray($array) {
+    foreach ($k->$v in ) {
+        echo "<dt>{$k}</dt><dd>\n";
+        if (is_array($v)) {
+            foreach ($k->$v
+}
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Current User's Session Data</title>
+</head>
+<body>
+<h1>Current User's Session data</title>
+<dl>
+<?displayarray($_SESSION[$sprefix]);?>
+</dl>
+
