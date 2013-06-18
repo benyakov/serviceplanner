dc7911a429f25252de27e38190091f4e6cee4416
Jesse Jacobsen <jmatjac@gmail.com>
2013-06-17 21:04:33 -0700

    Setting PDO Param values on churchyear data

--- utility/fillservicetables.php
+++ utility/fillservicetables.php
@@ -30,7 +30,6 @@ $q = $dbh->prepare("INSERT INTO {$dbp}churchyear
         observed_month, observed_sunday)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 $daynames = array();
-$propernames = array();
 while (($record = fgetcsv($fh)) != FALSE) {
     $r = array();
     foreach ($record as $field) {
@@ -79,12 +78,12 @@ $qp = $dbh->prepare("INSERT INTO {$dbp}churchyear_propers
     (dayname, color, theme, introit)
     VALUES (?, ?, ?, ?)");
 while (($record = fgetcsv($fh)) != FALSE) {
-    $r = blanksToNull($record);
-    $dict = array_combine($headings, $r);
-    $propernames[] = $dict['dayname'];
-    $qp->execute(array($dict['dayname'], $dict['color'],
-        $dict['theme'], $dict['introit']))
-        or die(__FILE__.":".__LINE__.$dict['dayname'].
+    $dict = array_combine($headings, $record);
+    $qp->bindValue(1, $dict['dayname'], paramStrNull($dict['dayname']));
+    $qp->bindValue(2, $dict['color'], paramStrNull($dict['color']));
+    $qp->bindValue(3, $dict['theme'], paramStrNull($dict['theme']));
+    $qp->bindValue(4, $dict['introit'], paramStrNull($dict['introit']));
+    $qp->execute() or die(__FILE__.":".__LINE__.$dict['dayname'].
             array_pop($qp->errorInfo()));
 }
 
@@ -96,13 +95,20 @@ $ql = $dbh->prepare("INSERT INTO {$dbp}churchyear_lessons
     s2lesson, s2gospel, s3lesson, s3gospel, hymnabc, hymn)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 while (($record = fgetcsv($fh)) != FALSE) {
-    $r = blanksToNull($record);
-    $dict = array_combine($headings, $r);
-    $ql->execute(array($dict['dayname'], $dict['type'], $dict['lesson1'],
-        $dict['lesson2'], $dict['gospel'], $dict['psalm'],
-        $dict['s2lesson'], $dict['s2gospel'], $dict['s3lesson'],
-        $dict['s3gospel'], $dict['hymnabc'], $dict['hymn']))
-        or die(__FILE__.":".__LINE__.$dict['dayname']);
+    $dict = array_combine($headings, $record);
+    $ql->bindValue(1, $dict['dayname'], paramStrNull($dict['dayname']));
+    $ql->bindValue(2, $dict['type'], paramStrNull($dict['type']));
+    $ql->bindValue(3, $dict['lesson1'], paramStrNull($dict['lesson1']));
+    $ql->bindValue(4, $dict['lesson2'], paramStrNull($dict['lesson2']));
+    $ql->bindValue(5, $dict['gospel'], paramStrNull($dict['gospel']));
+    $ql->bindValue(6, $dict['psalm'], paramStrNull($dict['psalm']));
+    $ql->bindValue(7, $dict['s2lesson'], paramStrNull($dict['s2lesson']));
+    $ql->bindValue(8, $dict['s2gospel'], paramStrNull($dict['s2gospel']));
+    $ql->bindValue(9, $dict['s3lesson'], paramStrNull($dict['s3lesson']));
+    $ql->bindValue(10, $dict['s3gospel'], paramStrNull($dict['s3gospel']));
+    $ql->bindValue(11, $dict['hymnabc'], paramStrNull($dict['hymnabc']));
+    $ql->bindValue(12, $dict['hymn'], paramStrNull($dict['hymn']));
+    $ql->execute() or die(__FILE__.":".__LINE__.$dict['dayname']);
 }
 
 // Fill collect text
@@ -112,10 +118,12 @@ $ql = $dbh->prepare("INSERT INTO {$dbp}churchyear_collects
     (id, class, collect)
     VALUES (?, ?, ?)");
 while (($record = fgetcsv($fh)) != FALSE) {
-    $r = blanksToNull($record);
-    $dict = array_combine($headings, $r);
-    $ql->execute(array($dict['id'], $dict['type'], $dict['collect']))
-        or die(__FILE__.":".__LINE__.$dict['dayname']);
+    $dict = array_combine($headings, $record);
+    $ql->bindValue(1, $dict['id'],
+        getParamType($dict['id'], PDO::PARAM_INT));
+    $ql->bindvalue(2, $dict['type'], paramStrNull($dict['type']));
+    $ql->bindvalue(3, $dict['collect'], paramStrNull($dict['collect']));
+    $ql->execute() or die(__FILE__.":".__LINE__.$dict['dayname']);
 }
 
 // Fill collect indexes
@@ -125,10 +133,12 @@ $ql = $dbh->prepare("INSERT INTO `{$dbp}churchyear_collect_index`
     (dayname, lectionary, id)
     VALUES (?, ?, ?)");
 while (($record = fgetcsv($fh)) != FALSE) {
-    $r = blanksToNull($record);
-    $dict = array_combine($headings, $r);
-    $ql->execute(array($dict['dayname'], $dict['type'], $dict['number']))
-        or die(__FILE__.":".__LINE__.$dict['dayname']);
+    $dict = array_combine($headings, $record);
+    $ql->bindValue(1, $dict['dayname'], paramStrNull($dict['dayname']));
+    $ql->bindValue(2, $dict['type'], paramStrNull($dict['number']));
+    $ql->bindValue(3, $dict['number'],
+        getParamType($dict['number'], PDO::PARAM_INT));
+    $ql->execute() or die(__FILE__.":".__LINE__.$dict['dayname']);
 }
 
 // Fill graduals
@@ -138,20 +148,19 @@ $ql = $dbh->prepare("INSERT INTO `{$dbp}churchyear_graduals`
     (season, gradual)
     VALUES (?, ?)");
 while (($record = fgetcsv($fh)) != FALSE) {
-    $r = blanksToNull($record);
-    $dict = array_combine($headings, $r);
-    $ql->execute(array($dict['season'], $dict['gradual']))
-        or die(__FILE__.":".__LINE__.$dict['season']);
+    $dict = array_combine($headings, $record);
+    $ql->bindValue(1, $dict['season'], paramStrNull($dict['season']));
+    $ql->bindValue(1, $dict['gradual'], paramStrNull($dict['gradual']));
+    $ql->execute() or die(__FILE__.":".__LINE__.$dict['season']);
 }
 
-function blanksToNull($arrayin) {
-    $r = array();
-    foreach ($arrayin as $field) {
-        $f = trim($field);
-        if (! $f) {
-            $f = PDO::PARAM_NULL;
-        }
-        $r[] = $f;
-    }
-    return $r;
+function getParamType($value, $nonnulltype) {
+    if ($value)
+        return $nonnulltype;
+    else
+        return PDO::PARAM_NULL;
+}
+
+function paramStrNull($value) {
+    getParamType($value, PDO::PARAM_STR);
 }
