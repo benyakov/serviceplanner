d7cea19065e68c3a49658228fac6f1042f4d55c5
Jesse Jacobsen <jmatjac@gmail.com>
2016-12-19 10:58:16 -0800

    Referencing jquery location as object properties

--- functions.php
+++ functions.php
@@ -442,10 +442,10 @@ function html_head($title, $xstylesheets=Array()) {
                 $rv[] = "<link type=\"text/css\" rel=\"stylesheet\" href=\"{$here}/styles/{$xstyle}\">";
             }
         }
-        $rv[] = "<script type=\"text/javascript\" src=\"{$jquery_locations['jquery']}\"></script>";
-        $rv[] = "<link href=\"{$jquery_locations['style']}\" rel=\"stylesheet\" type=\"text/css\"/>
+        $rv[] = "<script type=\"text/javascript\" src=\"{$jquery_locations->jquery}\"></script>";
+        $rv[] = "<link href=\"{$jquery_locations->style}\" rel=\"stylesheet\" type=\"text/css\"/>
         <script type=\"text/javascript\" src=\"modernizr/modernizr.js\"></script>
-        <script type=\"text/javascript\" src=\"{$jquery_locations['ui']}\"></script>
+        <script type=\"text/javascript\" src=\"{$jquery_locations->ui}\"></script>
         <script type=\"text/javascript\" src=\"jquery/jquery.ba-dotimeout.min.js\"></script>";
         $rv[] = "<script type=\"text/javascript\" src=\"{$here}/ecmascript.js.php\"></script>";
     }
