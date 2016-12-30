4cb575912330c9a8bff6b888f12669e9050a3d0d
Jesse Jacobsen <jmatjac@gmail.com>
2016-12-29 17:49:02 -0800

    Removed errant beginTransaction

--- utility/upgrades/0.16to0.17.php
+++ utility/upgrades/0.16to0.17.php
@@ -66,7 +66,6 @@ $q->execute() or die(array_pop($q->errorInfo()));
 $rm[] = "Removed communion column from days table.";
 
 // Change "location" to "occurrence"
-$db->beginTransaction();
 $q = $db->prepare("ALTER TABLE `{$db->getPrefix()}hymns`
     CHANGE `location` `occurrence` varchar(50) default NULL");
 $q->execute() or die(array_pop($q->errorInfo()));
